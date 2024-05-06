a9b390ccc305b6030ace9b6589047d465da70782
BUI DAN <btcdan789@gmail.com>
2024-05-06 20:11:31 +0700

    login_done

---
+++ control/xulydangnhap.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kết quả đăng nhập</title>
+</head>
+<body>
+    <?php 
+     $tk = $_GET['txttaikhoan'];
+        echo "Tài khoản là: " . $_GET['txttaikhoan'] . "</br> In bằng cách khai báo biến: ". $tk . "<br/>Mật khẩu là: " .$_GET['txtmatkhau'] ."<br/>"; 
+    ?>
+<a href="../login.php" >Quay lại trang Đăng nhập</a>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN FORM</title>
+</head>
+<body>
+    <h1>ĐĂNG NHẬP</h1>
+    <form id="login_from" name="login_form" method="get" action="control/xulydangnhap.php">
+    <label>Tài khoản</label>
+    <input type="text" name="txttaikhoan" id="txttaikhoan" />
+
+    <label>Mật khẩu</label>
+    <input type="password" name="txtmatkhau" id="txtmatkhau" />
+
+    <button type="submit">Đăng nhập</button>
+    </form>
+</body>
+</html>
