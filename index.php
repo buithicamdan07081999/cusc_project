23455e157e49471736271b041a738ed2bb4ffeb1
BUIDAN <btcdan789@gmail.com>
2024-05-07 00:48:25 +0700

    js

--- index.php
+++ index.php
@@ -1,23 +1,44 @@
-
-
 <!-- gõ html; chọn html:5 tab -->
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Xin chào PHP</title>
     <?php
-        echo 'Xin chào PHP';
-?>
-<br/>
-<a href="get_post/login.php">PHƯƠNG THỨC GET</a><br/>
-<a href="get_post/login2.php">PHƯƠNG THỨC POST</a><br/>
-<a href="get_post/ketquahoctap.php">KẾT QUẢ HỌC TẬP</a>
+    echo 'Xin chào PHP';
+    ?>
+    <br />
+    <span>HỌC PHP</span>
+    <ul>
+        <li>
+            <a href="get_post/login.php">PHƯƠNG THỨC GET</a>
+        </li>
+        <li>
+            <a href="get_post/login2.php">PHƯƠNG THỨC POST</a><br />
+        </li>
+        <a href="get_post/ketquahoctap.php">KẾT QUẢ HỌC TẬP</a>
+        <li>
+            <a href="get_post/login.php">PHƯƠNG THỨC GET</a>
+        </li>
+        <li>
+            <a href="get_post/ketquahoctap.php">KẾT QUẢ HỌC TẬP</a>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            <a href="js/hello.html">JavaScript Hello World!</a>
+        </li>
+    </ul>
+
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
---
+++ js/app.js
@@ -0,0 +1,2 @@
+// Gọi hàm alert('') để hiển thị thông báo
+alert('Chào mừng đến với JavaScript. Tôi là code JS trong file rời...');
