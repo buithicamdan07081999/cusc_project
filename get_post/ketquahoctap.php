af91b1af1f233d95976da1d71afbf455aa8e8579
BUI DAN <btcdan789@gmail.com>
2024-05-06 21:26:31 +0700

    radiobutton

--- get_post/ketquahoctap.php
+++ get_post/ketquahoctap.php
@@ -10,8 +10,10 @@
     <form name="ketquahoctap_form" id="ketquahoctap_form" method="post" action="../control/xulyketqua.php">
     <label>HỌ TÊN: </label>
     <input type="text" id="txthoten" name="txthoten"><br/>
-    <label>GIỚI TÍNH: </label>
-    <input type="text" id="txtgioitinh" name="txtgioitinh"><br/>
+    <label>GIỚI TÍNH: </label></br>
+    <input type="radio" id="txtgioitinh_nam" name="txtgioitinh" value="1" >Nam
+    <input type="radio" id="txtgioitinh_nu" name="txtgioitinh" value="2">Nữ
+    <br/><a>checked để chọn mặc định</a><br/>
     <label>ĐIỂM</label>
     <input type="number" id="txtdiem" name="txtdiem"><br/>
     <button type="submit">Tra cứu</button>
